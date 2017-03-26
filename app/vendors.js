80f7ca9ae6ce1e4536627f21f6ea36cce4feebcd
Shane Cavaliere <shane@shanecavaliere.com>
2017-03-26 00:37:57 -0600

    jest support in eslint/flow; updated vendors

--- app/vendors.js
+++ app/vendors.js
@@ -2,5 +2,8 @@
  * Used in Webpack DLL for faster builds (see tools/build/webpack.dll.js)
  */
 
+require("babel-polyfill");
 require("react");
 require("react-dom");
+require("smoothscroll-polyfill");
+require("styled-components");
